87bf458702cdac11f70fd17de79a0b2b8a47ae24
Alan <len40@student.douglascollege.ca>
2025-04-02 12:01:40 -0700

    Update main.cpp _replace "g." with "Graphics::"_

--- main.cpp
+++ main.cpp
@@ -11,8 +11,6 @@ int main()
 		if (!infile.is_open())
 			throw "Error! File could not be opened.";
 
-		Graphics g;
-
 		time_t start, end;
 
 		/* create images */
@@ -33,43 +31,43 @@ int main()
 		PPM* image14 = new PPM(*image0);
 
 		/* test filters */
-		g.applyFilter(*image1, "blur"); // blur image
+		Graphics::applyFilter(*image1, "blur"); // blur image
 		image1->saveImageToFile(filename + "_blur.ppm");
-		g.applyFilter(*image2, "sharpen"); // sharpen image
+		Graphics::applyFilter(*image2, "sharpen"); // sharpen image
 		image2->saveImageToFile(filename + "_sharpen.ppm");
-		g.applyFilter(*image3, "edgeDetect"); // detect edges
+		Graphics::applyFilter(*image3, "edgeDetect"); // detect edges
 		image3->saveImageToFile(filename + "_edgeDetect.ppm");
-		g.applyFilter(*image4, "emboss"); // emboss image
+		Graphics::applyFilter(*image4, "emboss"); // emboss image
 		image4->saveImageToFile(filename + "_emboss.ppm");
-		g.applyFilter(*image5, "magic"); // randomly-created filter (alternate)
+		Graphics::applyFilter(*image5, "magic"); // randomly-created filter (alternate)
 		image5->saveImageToFile(filename + "_magic.ppm");
 
 		/* test scaling */
-		g.scaleImage(*image6, 0.5); // shrink image
+		Graphics::scaleImage(*image6, 0.5); // shrink image
 		image6->saveImageToFile(filename + "_scaleDown.ppm");
-		g.scaleImage(*image7, 1.75); // expand image
+		Graphics::scaleImage(*image7, 1.75); // expand image
 		image7->saveImageToFile(filename + "_scaleUp.ppm");
-		g.scaleImage(*image8, -1.5); // reverse image
+		Graphics::scaleImage(*image8, -1.5); // reverse image
 		image8->saveImageToFile(filename + "_reverse.ppm");
 
 		/* test rotation */
-		g.rotateImage(*image9, -47.6); // rotate image CCW
+		Graphics::rotateImage(*image9, -47.6); // rotate image CCW
 		image9->saveImageToFile(filename + "_CCW_rotation.ppm");
-		g.rotateImage(*image10, 156.6); // rotate image CW
+		Graphics::rotateImage(*image10, 156.6); // rotate image CW
 		image10->saveImageToFile(filename + "_CW_roatation.ppm");
 
 		/* test translation */
-		g.translateImage(*image11, 300, -100);// tranlate image right and down
+		Graphics::translateImage(*image11, 300, -100);// tranlate image right and down
 		image11->saveImageToFile(filename + "_translateRD.ppm");
-		g.translateImage(*image12, -150, 250);// translate image left and up
+		Graphics::translateImage(*image12, -150, 250);// translate image left and up
 		image12->saveImageToFile(filename + "_translateLU.ppm");
 
 		/* test grayscale */
-		g.makeGrayscale(*image13);//convert image to grayscale
+		Graphics::makeGrayscale(*image13);//convert image to grayscale
 		image13->saveImageToFile(filename + "_grayScale.ppm");
 
 		/* test invertColor */
-		g.invertColors(*image14);//convert image to grayscale
+		Graphics::invertColors(*image14);//convert image to grayscale
 		image14->saveImageToFile(filename + "_invertColors.ppm");
 
 		/* delete images */
